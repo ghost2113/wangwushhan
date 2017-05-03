3bbb3209098fdd2c3d717ab54c39c9e3979d8d80
admin <tianjinying93@163.com>
2017-05-03 18:13:43 +0800

    first commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 $(function(){
+/*首页*/
 	//内容详情列表图片切换
 	$(".main_left_con li").mouseover(function(){
 		//console.log($(this).attr("data_src"))
 		var img_src = $(this).attr("data_src");
 		$(".main_left_img img:eq(0)").attr("src",img_src)
 	})
+	//选项卡
+	$(".main_right_title li").click(function(){
+
+		//console.log($(this).index())
+		var index = $(this).index();
+		console.log($(".t_online_con form").eq(index))
+		$(this).addClass("main_right_title_active").siblings().removeClass("main_right_title_active");
+		$(".t_online_con form").eq(index).css("display","block").siblings().css("display","none");
+	})
+	$(".main_right_title li").eq(0).trigger("click");
 	//导航页面
 	$(".header_nav2 li").click(function(){
 		$(".header_nav2 li").eq($(this).index()).children(".level2").stop().animate({"width":"0.3rem"});
@@ -12,7 +23,8 @@ $(function(){
 		 	"border-top":"5px solid #8c8c8c",
     		"border-radius":"0 0 0.1rem 0.1rem",
     		"color":"white",
-    		"background":"#1bba9e url(../images/nav_bg.png) no-repeat right bottom"
+    		"background":"rgba(0,0,0,.7) no-repeat right bottom",
+    		"background-image":"url(../images/nav_bg.png)"
     	})
 		$(".header_nav2 li").eq($(this).index()).children("a").css({"color":"#fff"});
 		
@@ -48,6 +60,30 @@ $(function(){
 			$(".food_con").eq($(this).index()).stop().animate({"top":"100%"},1000)
 		})
 	})
+	//手风琴效果实现
+/*	$(".ol_order_right li").on("mouseenter",function(){
+		//console.log($(this).css("width"))
+		var index = $(this).index();
+		$(this).stop("true","true").animate({"width":"40%"},500).siblings().stop("true","true").animate({"width":"20%"},500);
+	})
+	*/
+	/***************************************
+					hover事件的使用:$(selector).hover(fn1, fn2);
+					鼠标进入，执行函数fn1
+					鼠标移出，执行函数fn2
+
+					扩展：$(selector).find(expr)：在选中的元素中查询满足expr选择器选中的元素
+				***************************************/
+				$(".ol_order_right li").hover(function(){
+					/* 当前图片的宽度自定义动画结果400，其他的宽度动画结果160，整体宽度不变*/
+					$(this).css("width","48%").siblings().css("width","13%");
+					$(this).find(".des").css("display","none");
+					$(this).find(".scenic_info").css("display","block");
+				},function(){
+					$(this).css("width","20%").siblings().css("width","20%");
+					$(this).find(".des").css("display","block");
+					$(this).find(".scenic_info").css("display","none");
+				});
 	
 	/**
 	 * 	二级页面 选项卡效果实现
@@ -84,5 +120,5 @@ $(function(){
 		var count = 0;
 		$(".t_attractionsbox_position").animate({"left":direc*width},500)
 	}
-})
 
+})
